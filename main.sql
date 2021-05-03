9f0b34c548a9ad47417cef06613701ebbe8f4f4a
nhporti2 <nhporti2@gmail.com>
2021-05-02 22:26:35 -0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,69 +1,64 @@
 -- 1. How many actors are there with the last name ‘Wahlberg’? --
 
-SELECT COUNT(last_name)
+SELECT last_name
 FROM actor
 WHERE last_name = 'Wahlberg';
 
 -- 2. How many payments were made between $3.99 and $5.99? --
 
-SELECT COUNT(payment_id)
+SELECT payment_id
 FROM payment
 WHERE amount BETWEEN 3.99 AND 5.99;
 
 -- 3. What film does the store have the most of? (search in inventory) --
 
-SELECT film_id, COUNT(film_id)
+SELECT film_id
 FROM inventory
 GROUP BY film_id
 ORDER BY film_id;
 
 -- 4. How many customers have the last name ‘William’? --
 
-SELECT COUNT(last_name)
+SELECT last_name
 FROM customer
 WHERE last_name = 'William';
 
 -- 5. What store employee (get the id) sold the most rentals? --
 
-SELECT staff_id, COUNT(rental_ID)
+SELECT staff_id
 FROM rental
 GROUP BY staff_id
 ORDER BY COUNT(rental_ID) DESC;
 
 -- 6. How many different district names are there? --
 
-SELECT COUNT(DISTINCT district)
+SELECT COUNT(district)
 FROM address;
 
 -- 7. What film has the most actors in it? (use film_actor table and get film_id) --
-SELECT film_id, COUNT(actor_id)
+SELECT actor_id
 FROM film_actor
 GROUP BY film_id
 ORDER BY COUNT(actor_id) DESC;
 
 -- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table) --
 
-SELECT store_id, COUNT(last_name)
+SELECT customer_id, store_id, last_name
 FROM customer
 WHERE last_name LIKE '%es'
-GROUP BY store_id
-ORDER BY store_id;
 
 -- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers with ids between 380 and 430? (use group by and having > 250) --
 
-SELECT amount, COUNT(rental_id)
+SELECT amount, COUNT(payment_id)
 FROM payment
-WHERE customer_id BETWEEN 380 AND 430
+WHERE customer_id BETWEEN 380 and 430
 GROUP BY amount
-HAVING COUNT(rental_id) >250
-ORDER BY COUNT(rental_id) DESC;
+HAVING COUNT(payment_id) > 250;
 
 -- 10. Within the film table, how many rating categories are there? And what rating has the most movies total? --
 
-SELECT COUNT(DISTINCT rating)
+SELECT COUNT(rating)
 FROM film;
-
 SELECT rating, COUNT(film_id)
 FROM film
 GROUP BY rating
-ORDER BY COUNT(film_id) DESC;
